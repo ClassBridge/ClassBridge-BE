da69dc2547054a36c43e01f886c18240cb0367a5
JaeJun <yyggb12@kakao.com>
2024-05-28 08:50:25 +0900

    chore(domain): add Hello.java
    - with BaseEntity
    - rename package model to domain

---
+++ src/main/java/com/linked/classbridge/domain/BaseEntity.java
@@ -0,0 +1,31 @@
+package com.linked.classbridge.domain;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@MappedSuperclass
+@SuperBuilder(builderMethodName = "doesNotUseThisBuilder")
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    private LocalDateTime deletedAt;
+}
