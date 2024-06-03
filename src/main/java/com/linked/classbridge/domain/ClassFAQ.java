d01fc0a4b70df6671caef5edf9d8c904e0c7c3c0
younggeun <chldudrms94@naver.com>
2024-06-03 17:41:22 +0900

    feat: faq 관련 entity, repository, type 추가

---
+++ src/main/java/com/linked/classbridge/domain/ClassFAQ.java
@@ -0,0 +1,40 @@
+package com.linked.classbridge.domain;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+
+@Entity(name = "class_faq")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@SuperBuilder
+@SQLRestriction("deleted_at is null")
+@SQLDelete(sql = "UPDATE class_faq SET deleted_at = NOW() WHERE faq_id = ?")
+@ToString
+public class ClassFAQ extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long faqId;
+
+    private String title;
+    private String content;
+    private int sequence;
+
+    @JoinColumn(name = "class_id")
+    @ManyToOne
+    private OneDayClass oneDayClass;
+}
