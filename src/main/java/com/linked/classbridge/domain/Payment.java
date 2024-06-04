d1c8a7c95bd1c10a3e07f8a32b3463b0719fcbe4
SoieYoo <yoosoie0000@gmail.com>
2024-06-04 20:15:02 +0900

    Save current changes commit.

---
+++ src/main/java/com/linked/classbridge/domain/Payment.java
@@ -0,0 +1,68 @@
+package com.linked.classbridge.domain;
+
+import com.linked.classbridge.dto.payment.PaymentPrepareDto;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+public class Payment extends BaseEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long paymentId;
+
+    //        @Column(nullable = false, length = 10)
+    private String cid;
+
+    //        @Column(nullable = false, length = 100)
+    private String partnerOrderId;
+
+    //        @Column(nullable = false, length = 100)
+    private String partnerUserId;
+
+    //        @Column(nullable = false, length = 100)
+    private String itemName;
+
+    //        @Column(length = 100)
+    private String itemCode;
+
+    //        @Column(nullable = false)
+    private Long quantity;
+
+    //        @Column(nullable = false)
+    private int totalAmount;
+
+    //        @Column(length = 255)
+    private String paymentMethodType;
+
+    private Integer installMonth;
+
+    //        @Column(nullable = false, length = 20)
+    private String tid;
+
+    //        @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    //        @Column(nullable = false, length = 255, columnDefinition = "varchar(255) default 'PENDING'")
+    private String status;
+
+    public static Payment convertToPaymentEntity(PaymentPrepareDto.Request request) {
+        return Payment.builder()
+                .cid(request.getCid())
+                .tid(request.getTid())
+                .partnerOrderId(request.getPartnerOrderId())
+                .partnerUserId(request.getPartnerUserId())
+                .status("COMPLETED")
+                .build();
+    }
+}
--- src/main/java/com/linked/classbridge/dto/payment/PaymentPrepareDto.java
+++ src/main/java/com/linked/classbridge/dto/payment/PaymentPrepareDto.java
@@ -13,16 +13,20 @@ public class PaymentPrepareDto {
         private int quantity;               // 상품 수량
         private int totalAmount;            // 상품 총액
         private int texFreeAmount;          // 상품 비과세 금액
+        private String pgToken;
+        private String cid;
+        private String tid;
     }
 
     @Getter
+    @Setter
     public static class Response {
+        private String cid;
         private String partnerOrderId;      // 가맹점 주문 번호
         private String partnerUserId;       // 가맹점 회원 id
         private String tid;                 // 결제 고유 번호
         private String next_redirect_pc_url;// pc 웹일 경우 받는 결제 페이지
         private String created_at;
-        @Setter
         private String pgToken;
     }
 }
