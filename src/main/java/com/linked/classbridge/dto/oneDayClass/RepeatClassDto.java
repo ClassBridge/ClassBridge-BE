6943a01b3f6ef95cc77c02b8eb14258eb038fb35
younggeun <chldudrms94@naver.com>
2024-06-07 23:51:24 +0900

    refactor: create repeat lesson refactoring

--- src/main/java/com/linked/classbridge/dto/oneDayClass/RepeatClassDto.java
+++ src/main/java/com/linked/classbridge/dto/oneDayClass/RepeatClassDto.java
@@ -1,5 +1,6 @@
 package com.linked.classbridge.dto.oneDayClass;
 
+import java.time.DayOfWeek;
 import java.time.LocalTime;
 import java.util.List;
 import lombok.AllArgsConstructor;
@@ -16,20 +17,6 @@ import lombok.ToString;
 @Builder
 @ToString
 public class RepeatClassDto {
-    private dayList mon;
-    private dayList tue;
-    private dayList wed;
-    private dayList thu;
-    private dayList fri;
-    private dayList sat;
-    private dayList sun;
-
-    @Getter
-    @Setter
-    @Builder
-    @NoArgsConstructor
-    @AllArgsConstructor
-    public static class dayList {
-        private List<LocalTime> times;
-    }
+    private DayOfWeek dayOfWeek;
+    private List<LocalTime> times;
 }
