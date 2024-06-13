7f6c17f9e23d0fb82d9ec8614f9e637324dcaa91
SoieYoo <yoosoie0000@gmail.com>
2024-06-13 19:23:14 +0900

    refactor : 결제 시 예약 가능 여부 확인하도록 수정

--- src/main/java/com/linked/classbridge/config/SecurityConfig.java
+++ src/main/java/com/linked/classbridge/config/SecurityConfig.java
@@ -10,7 +10,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import java.util.Collections;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -87,7 +86,11 @@ public class SecurityConfig {
         //경로별 인가 작업
         http
                 .authorizeHttpRequests((auth) -> auth
-                        .requestMatchers("/", "/api/users/auth/**", "/swagger-ui/*", "/v3/api-docs/**").permitAll()
+                        .requestMatchers("/swagger-ui/*", "/v3/api-docs/**").permitAll()
+                        .requestMatchers("/", "/api/users/auth/**").permitAll()
+                        .requestMatchers("/api/users/auth/reissue").permitAll()
+                        .requestMatchers("api/tutors/**").permitAll()
+                        .requestMatchers("/CB-websocket/**").permitAll()
                         .requestMatchers("/api/users").hasRole("USER")
                         .requestMatchers("/api/users/**").hasRole("USER")
                         .anyRequest().authenticated())
