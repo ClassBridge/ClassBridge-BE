0ae6f53d43883bfb9bb3ea20f28715dd807c69da
younggeun <chldudrms94@naver.com>
2024-06-16 19:13:37 +0900

    refactor: add @Repository

--- src/main/java/com/linked/classbridge/repository/OneDayClassDocumentRepository.java
+++ src/main/java/com/linked/classbridge/repository/OneDayClassDocumentRepository.java
@@ -2,7 +2,9 @@ package com.linked.classbridge.repository;
 
 import com.linked.classbridge.domain.document.OneDayClassDocument;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface OneDayClassDocumentRepository extends ElasticsearchRepository<OneDayClassDocument, Long> {
 
 }
