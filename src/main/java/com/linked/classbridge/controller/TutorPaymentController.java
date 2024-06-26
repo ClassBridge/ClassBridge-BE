caabc29161c42d625c4b993196f1b3588f398cc7
SoieYoo <yoosoie0000@gmail.com>
2024-06-26 20:32:03 +0900

    refactor: 메서드 별 user/tutor 권한 추가

--- src/main/java/com/linked/classbridge/controller/TutorPaymentController.java
+++ src/main/java/com/linked/classbridge/controller/TutorPaymentController.java
@@ -4,11 +4,13 @@ import com.linked.classbridge.dto.SuccessResponse;
 import com.linked.classbridge.dto.tutorPayment.TutorPaymentResponse;
 import com.linked.classbridge.service.TutorPaymentService;
 import com.linked.classbridge.type.ResponseMessage;
+import io.swagger.v3.oas.annotations.Operation;
 import java.time.YearMonth;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,6 +23,8 @@ public class TutorPaymentController {
 
     private final TutorPaymentService tutorPaymentService;
 
+    @Operation(summary = "강사 정산 조회", description = "강사 정산 내역을 조회합니다.")
+    @PreAuthorize("hasRole('TUTOR')")
     @GetMapping("/payments")
     public ResponseEntity<SuccessResponse<List<TutorPaymentResponse>>> getTutorPayments(
             @RequestParam(required = false) String yearMonth) {
--- src/main/java/com/linked/classbridge/controller/TutorSalesController.java
+++ src/main/java/com/linked/classbridge/controller/TutorSalesController.java
@@ -2,7 +2,9 @@ package com.linked.classbridge.controller;
 
 import com.linked.classbridge.dto.sales.TutorSalesResponse;
 import com.linked.classbridge.service.SalesService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,6 +16,8 @@ public class TutorSalesController {
 
     private final SalesService salesService;
 
+    @Operation(summary = "강사 매출 조회", description = "강사 매출 내역을 조회합니다.")
+    @PreAuthorize("hasRole('TUTOR')")
     @GetMapping("/sales")
     public TutorSalesResponse getTutorSales(@RequestParam int year) {
         return salesService.getSalesData(year);
